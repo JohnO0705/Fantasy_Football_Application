25573122edb5dbd8cc26bd6a978c07b40b331f2e
John Ortiz <john.ortiz0705@gmail.com>
2025-08-26 01:26:29 -0500

    added app

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import base64
 from flask import Flask, request, redirect
 import requests
 
+app = Flask(__name__)
 
 client_id = 'dj0yJmk9SzgxeHBIOW4xc2NXJmQ9WVdrOVpVUnhhRXAzUW04bWNHbzlNQT09JnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PTMw'
 client_secret = 'b5513f001693afa6d7330adb782feb37062ac03e'
